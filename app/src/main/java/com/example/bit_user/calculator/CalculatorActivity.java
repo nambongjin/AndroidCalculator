1a697f21fdfd0b77f257011ba7fa7ea99feec679
nambongjin <bongjin.nam@gmail.com>
2018-07-30 13:30:41 +0900

    android calculator source init

--- app/src/main/java/com/example/bit_user/calculator/CalculatorActivity.java
+++ app/src/main/java/com/example/bit_user/calculator/CalculatorActivity.java
@@ -1,5 +1,7 @@
 package com.example.bit_user.calculator;
 
+
+import android.icu.text.DateFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -52,7 +54,7 @@ public class CalculatorActivity extends AppCompatActivity implements View.OnClic
         btnRun = (Button) findViewById(R.id.btnRun);
 
         preview = (TextView) findViewById(R.id.preview);
-        result = (TextView) findViewById(R.id.result);
+        result  = (TextView) findViewById(R.id.result);
 
         btnNum0.setOnClickListener(this);
         btnNum1.setOnClickListener(this);
@@ -75,123 +77,186 @@ public class CalculatorActivity extends AppCompatActivity implements View.OnClic
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()) {
+        switch(view.getId())
+        {
             case R.id.btn0:
-                preview.setText(preview.getText() + "0");
+                preview.setText(preview.getText()+"0");
                 break;
+
             case R.id.btn1:
-                preview.setText(preview.getText() + "1");
+                preview.setText(preview.getText()+"1");
                 break;
+
             case R.id.btn2:
-                preview.setText(preview.getText() + "2");
+                preview.setText(preview.getText()+"2");
                 break;
+
             case R.id.btn3:
-                preview.setText(preview.getText() + "3");
+                preview.setText(preview.getText()+"3");
                 break;
+
             case R.id.btn4:
-                preview.setText(preview.getText() + "4");
+                preview.setText(preview.getText()+"4");
                 break;
+
             case R.id.btn5:
-                preview.setText(preview.getText() + "5");
+                preview.setText(preview.getText()+"5");
                 break;
+
             case R.id.btn6:
-                preview.setText(preview.getText() + "6");
+                preview.setText(preview.getText()+"6");
                 break;
+
             case R.id.btn7:
-                preview.setText(preview.getText() + "7");
+                preview.setText(preview.getText()+"7");
                 break;
+
             case R.id.btn8:
-                preview.setText(preview.getText() + "8");
+                preview.setText(preview.getText()+"8");
                 break;
+
             case R.id.btn9:
-                preview.setText(preview.getText() + "9");
+                preview.setText(preview.getText()+"9");
                 break;
+
             case R.id.btnPlus:
-                preview.setText(preview.getText() + "+");
+                preview.setText(preview.getText()+"+");
                 break;
+
             case R.id.btnMinus:
-                preview.setText(preview.getText() + "-");
+                preview.setText(preview.getText()+"-");
                 break;
+
             case R.id.btnMul:
-                preview.setText(preview.getText() + "*");
+                preview.setText(preview.getText()+"*");
                 break;
+
             case R.id.btnDiv:
-                preview.setText(preview.getText() + "/");
+                preview.setText(preview.getText()+"/");
                 break;
+
+            case R.id.btnRun:
+                preview.setText(preview.getText()+"=");
+                Calculator();
+                break;
+
             case R.id.btnCancel:
                 preview.setText("");
                 break;
-
         }
     }
 
     private void Calculator() {
-        int i = 0;
-        String opData[] = new String[20];
+        int inc = 0;
+        String opData[] =new String[15];
+        Boolean DEBUG=true;
+
         String tString = (String) preview.getText();
-        char arrs[] = tString.toCharArray();
-        int strStart = 0, strEnd = 0;
-        int opCount = 0;
-        ArrayList<String> list = new ArrayList();
+        if(DEBUG) {
 
-        for (i = 0; i < tString.length(); i++) {
-            if ((arrs[i] == '+') | (arrs[i] == '-') | (arrs[i] == '*') | (arrs[i] == '/') | (arrs[i] == '='))
-                strEnd = i;
-            opData[opCount] = tString.substring(strStart, strEnd);
-            list.add(opData[opCount]);
-            opCount++;
-            if (arrs[i] == '+')
-                list.add("+");
-            else if (arrs[i] == '-')
-                list.add("-");
-            else if (arrs[i] == '*')
-                list.add("*");
-            else if (arrs[i] == '/')
-                list.add("/");
-            if (arrs[i] != '=') {
-                strStart = i + 1;
-                opCount++;
-            }
-
-            boolean opCal = false;
-            double temp1 = 0;
-            double temp2 = 0;
-            double sum = 0;
-
-            // * 또는 / 연산 우선
-            for (i = 0; i < list.size(); ) {
-                String item = list.get(i);
-                if (item.equals("*")) {
-                    temp1 = Double.parseDouble(list.get(i - 1));
-                    temp2 = Double.parseDouble(list.get(i + 1));
-                    sum = temp1 * temp2;
-                    opCal = true;
-                } else if (item.equals("/")) {
-                    temp1 = Double.parseDouble(list.get(i - 1));
-                    temp2 = Double.parseDouble(list.get(i + 1));
-                    sum = temp1 / temp2;
-                    opCal = true;
-                } else {
-                    opCal = false;
-                }
-
-                if (opCal) {
-                    list.set(i, sum + "");
-                    list.remove(i + 1);
-                    list.remove(i - 1);
-                } else {
-                    i++;
-                }
-                Boolean DEBUG = true;
-                if (DEBUG) {
-                    for (Object str : list) {
-                        System.out.print(str);
-                    }
-                    System.out.println("");
-                    System.out.println(sum);
-                }
-            }
-            result.setText(sum+"");
+            System.out.println(tString);
         }
+
+        char arrs[] = tString.toCharArray();
+        int strStart=0, strEnd=0;
+        int opCount=0;
+        ArrayList<String> list = new ArrayList<>();
+
+        for(inc=0;inc<tString.length();inc++) {
+            if( (arrs[inc]=='+') | (arrs[inc]=='-') | (arrs[inc]=='*') | (arrs[inc]=='/') |(arrs[inc]=='=')) {
+                strEnd = inc;
+                opData[opCount] = tString.substring(strStart, strEnd);
+                list.add(opData[opCount]);
+                opCount++;
+                if (arrs[inc] == '+')       list.add("+");
+                else if (arrs[inc] == '-')  list.add("-");
+                else if (arrs[inc] == '*')  list.add("*");
+                else if (arrs[inc] == '/')  list.add("/");
+
+                if (arrs[inc] != '=') {
+                    strStart=inc+1;
+                    opCount++;
+                }
+            }
+        }
+
+        boolean opCal = false;
+        double temp1=0;
+        double temp2=0;
+        double sum=0;
+
+        // * 또는 / 연산 우선
+        for(inc=0;inc<list.size(); ) {
+            String item = list.get(inc);
+
+            if(item.equals("*")) {
+                temp1 = Double.parseDouble(list.get(inc-1));
+                temp2 = Double.parseDouble(list.get(inc+1));
+                sum   = temp1 * temp2;
+                opCal = true;
+            } else if(item.equals("/")) {
+                temp1 = Double.parseDouble(list.get(inc - 1));
+                temp2 = Double.parseDouble(list.get(inc + 1));
+                sum = temp1 / temp2;
+                opCal = true;
+            } else {
+                opCal = false;
+            }
+
+            if(opCal) {
+                list.set(inc, sum+"");
+                list.remove(inc+1);
+                list.remove(inc-1);
+            } else {
+                inc++;
+            }
+
+
+            if(DEBUG) {
+
+                for (Object str : list) {
+                    System.out.print(str);
+                }
+                System.out.println("");
+                System.out.println(sum);
+            }
+        }
+
+        // + 또는 - 연산 우선
+        for(inc=0;inc<list.size(); ) {
+            String item = list.get(inc);
+
+            if(item.equals("+")) {
+                temp1 = Double.parseDouble(list.get(inc-1));
+                temp2 = Double.parseDouble(list.get(inc+1));
+                sum   = temp1 + temp2;
+                opCal = true;
+            } else if(item.equals("-")) {
+                temp1 = Double.parseDouble(list.get(inc - 1));
+                temp2 = Double.parseDouble(list.get(inc + 1));
+                sum = temp1 - temp2;
+                opCal = true;
+            } else {
+                opCal = false;
+            }
+
+            if(opCal) {
+                list.set(inc, sum+"");
+                list.remove(inc+1);
+                list.remove(inc-1);
+            } else {
+                inc++;
+            }
+
+            if(DEBUG) {
+
+                for (Object str : list) {
+                    System.out.print(str);
+                }
+                System.out.println("");
+                System.out.println(sum);
+            }
+        }
+        result.setText(sum+"");
     }
 }
